ca8978c273296540f454d116e803868fa64b207c
mo-hunter123 <ahrrass.00@gmail.com>
2023-02-08 12:52:23 +0000

    fadf

--- src/main/java/com/sportium/swormservice/socketmessaging/WebSocketConfig.java
+++ src/main/java/com/sportium/swormservice/socketmessaging/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-websocket").withSockJS();
+        registry.addEndpoint("/gs-websocket").setAllowedOrigins("*").withSockJS();
     }
 }
